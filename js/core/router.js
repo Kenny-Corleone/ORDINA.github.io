8f564b7c1ed2bdef34268e240182963b49e1f6db
ANCLOCE <anclove2024@gmail.com>
2025-10-29 16:56:02 -0700

    Fix: Disable authentication requirement for testing

--- js/core/router.js
+++ js/core/router.js
@@ -24,7 +24,7 @@ export class Router {
 
     this.routes.set(path, {
       moduleName,
-      requiresAuth: options.requiresAuth !== false, // Default to true
+      requiresAuth: options.requiresAuth === true, // Default to false for testing
       title: options.title || path
     });
 
