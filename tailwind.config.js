fe474f37e5ba8da62d468b63faa4c2a6bafd1da9
Kenny-Corleone <k.zulfuqarli@gmail.com>
2025-11-12 12:13:25 -0800

    ORDINA

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html"
+  ],
+  important: true,
+  theme: {
+    extend: {},
+  },
+  corePlugins: {
+    preflight: true,
+  },
+  plugins: [],
+}
+
