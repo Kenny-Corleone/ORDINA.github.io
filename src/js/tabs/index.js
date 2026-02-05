796550eb2b5afb0e6d0e3162260f61ed3b8a7c87
Kenny-Corleone <k.zulfuqarli@gmail.com>
2026-02-05 15:15:43 -0800

    я

--- src/js/tabs/index.js
+++
@@ -1,8 +0,0 @@
-// Export all tab modules
-export * from './debts.js';
-export * from './recurring-expenses.js';
-export * from './expenses.js';
-export * from './tasks.js';
-export * from './calendar.js';
-export * from './dashboard.js';
-
